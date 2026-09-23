8f7c213451094bd428e46d5c71a697dcdca954e2
Sebastian Kolczynski <kolczynskis17@gmail.com>
2026-09-22 20:20:09 -0500

    Add GPS module

--- include/gps.h
+++ include/gps.h
@@ -1,1 +1,12 @@
 #pragma once
+
+struct GPSData {
+    bool fix;
+    double latitude;
+    double longitude;
+    int time;
+};
+
+void gps_init();
+void gps_update();
+GPSData gps_get_data();
--- src/gps.cpp
+++ src/gps.cpp
@@ -1,1 +1,46 @@
+#include <Arduino.h>
+#include <Adafruit_GPS.h>
+#include <cstring>
 #include "gps.h"
+
+static Adafruit_GPS gps(&Serial1);
+static GPSData currentGPS;
+
+void gps_init()
+{
+    gps.begin(9600);
+    gps.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+    gps.sendCommand(PMTK_SET_NMEA_UPDATE_2HZ);
+}
+
+void gps_update()
+{
+    gps.read();
+
+    if (!gps.newNMEAreceived()) {
+        return;
+    }
+
+    char* sentence = gps.lastNMEA();
+
+    if (strncmp(sentence, "$GNRMC,", 7) != 0) {
+        return;
+    }
+
+    if(!gps.parse(sentence)) {
+        return;
+    }
+
+    currentGPS.fix = gps.fix;
+
+    if (currentGPS.fix) {
+        currentGPS.latitude = gps.latitudeDegrees;
+        currentGPS.longitude = gps.longitudeDegrees;
+        currentGPS.time = gps.hour * 10000 + gps.minute * 100 + gps.seconds;
+    }
+}
+
+GPSData gps_get_data()
+{
+    return currentGPS;
+}
