28b74a601d6194ed84ffeaf76c3925c71187460d
tfune <trevor.fune@gmail.com>
2026-09-25 15:24:45 -0700

    Complete tracking screen and update member list layout

--- include/display.h
+++ include/display.h
@@ -3,3 +3,4 @@
 bool displayInit();
 void displayStartup();
 void displayMemberList();
+void displayTracking(float distance, float direction);
--- src/display.cpp
+++ src/display.cpp
@@ -10,6 +10,60 @@ constexpr int DISPLAY_ADDRESS = 0x3C;
 
 static Adafruit_SH1106G display(DISPLAY_WIDTH, DISPLAY_HEIGHT, &Wire);
 
+static void drawDirectionArrow(float angle) {
+    const int centerX = 96;
+    const int centerY = 31;
+    const int length = 16;
+    const int headLength = 5;
+    const float headAngle = 30.0;
+
+    float radians = angle * PI / 180.0;
+
+    int endX = centerX + round(length * sin(radians));
+    int endY = centerY - round(length * cos(radians));
+
+    display.drawLine(centerX, centerY, endX, endY, SH110X_WHITE);
+
+    float leftRadians = (angle + 180.0 - headAngle) * PI / 180.0;
+    float rightRadians = (angle + 180.0 + headAngle) * PI / 180.0;
+
+    int leftX = endX + round(headLength * sin(leftRadians));
+    int leftY = endY - round(headLength * cos(leftRadians));
+
+    int rightX = endX + round(headLength * sin(rightRadians));
+    int rightY = endY - round(headLength * cos(rightRadians));
+
+    display.drawLine(endX, endY, leftX, leftY, SH110X_WHITE);
+    display.drawLine(endX, endY, rightX, rightY, SH110X_WHITE);
+}
+
+static const char* getCardinalDirection(float angle) {
+    if(angle >= 337.5 || angle < 22.5) {
+        return "N";
+    }
+    else if(angle < 67.5) {
+        return "NE";
+    }
+    else if(angle < 112.5) {
+        return "E";
+    }
+    else if(angle < 157.5) {
+        return "SE";
+    }
+    else if(angle < 202.5) {
+        return "S";
+    }
+    else if(angle < 247.5) {
+        return "SE";
+    }
+    else if(angle < 292.5) {
+        return "W";
+    }
+    else {
+        return "NW";
+    }
+}
+
 bool displayInit() {
     if(!display.begin(DISPLAY_ADDRESS)) {
         return false;
@@ -50,19 +104,48 @@ void displayMemberList() {
     display.setTextSize(1);
     display.setTextColor(SH110X_WHITE);
 
-    display.setCursor(28, 3);
+    display.setCursor(0, 0);
     display.print("SELECT MEMBER");
 
-    display.drawLine(10, 13, 117, 13, SH110X_WHITE);
+    display.drawLine(0, 10, 127, 10, SH110X_WHITE);
 
-    display.setCursor(10, 18);
+    display.setCursor(0, 16);
     display.print("> Sebastian");
 
-    display.setCursor(10, 28);
+    display.setCursor(0, 28);
     display.print("  Professor Salemi");
 
-    display.setCursor(10, 48);
-    display.print("Press to Select");
+    display.display();
+}
+
+void displayTracking(float distance, float direction) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SH110X_WHITE);
+
+    display.setCursor(0, 0);
+    display.print("SEBASTIAN");
+
+    display.drawLine(0, 10, 127, 10, SH110X_WHITE);
+
+    display.setCursor(0, 15);
+    display.print("Distance:");
+
+    display.setCursor(0, 25);
+    display.print(distance, 0);
+    display.print(" m");
+
+    display.setCursor(0, 39);
+    display.print("Direction:");
+
+    display.setCursor(0, 49);
+    display.print(direction, 0);
+    display.print(" deg");
+
+    drawDirectionArrow(direction);
+
+    display.setCursor(92, 52);
+    display.print(getCardinalDirection(direction));
 
     display.display();
 }
